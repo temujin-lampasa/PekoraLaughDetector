1d16f462c5c5eb2de65ba6f4affb74f87461768d
Temujin U. Lampasa <temujin.lampasa@obf.ateneo.edu>
2021-02-05 17:54:21 +0800

    Downsample to 16k Hz while converting mp4 to wav with ffmpeg

--- PekoraLaughDetector/convert.py
+++ PekoraLaughDetector/convert.py
@@ -2,15 +2,15 @@ import ffmpeg
 from clean import get_first_filename
 import os
 
-def convert_vid_to_wav(src_root="video_input", vid_fn = None):
+def convert_vid_to_wav(src_root="video_input", vid_fn = None, sr=16_000):
     valid_extensions = (".mp4", ".mkv")
     if not vid_fn:
         vid_fn = get_first_filename(src_root, valid_extensions)
 
     vid_path = os.path.join(src_root, vid_fn)
 
-    print(vid_path)
+    print(f"Converting {vid_path} to wav...")
 
     input_vid = ffmpeg.input(vid_path)
-    output_wav = ffmpeg.output(input_vid, vid_path.split(".")[0] + ".wav")
+    output_wav = ffmpeg.output(input_vid, vid_path.split(".")[0] + ".wav", ar=sr)
     output_wav.run()
